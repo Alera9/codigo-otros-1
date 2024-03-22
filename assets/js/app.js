10ac3587b134c44acd0eff65bca0f2748920d24d
Alera9 <mcarrazanag@gmail.com>
2024-03-22 12:17:17 -0600

    Fix: se hacen correcciones sobre el archivo js, se corrigen los selectores, se modificcan comillas simples por backticks, se agrega un elemento paragraph para indicar la unicación dentro de el, se corrige la funcion para obtener el usuario con el async fetch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,25 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
-
-function displayUser(username) {
+const $n = document.querySelector('.name');//se corrige el selector
+const $b = document.querySelector('.blog');//se corrige el selector de clase
+const $l = document.querySelector('.location');//se identifica que no existe tal elemento en el archivo HTML
+console.log($n,$b,$l);//Se hace uso de un console.log() para ver el funcionamiento de las variables
+async function displayUser(username) {//Se agrega la palabra async
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const data =await response.json(); //Se agrega la respuesta de la promesa en response
+   console.log(data);
+    //para las siguientes lines se cambian las comillas simples por backticks
+    $n.textContent = `${data.name}`;
+    $b.textContent = `${data.blog}`;
+    $l.textContent = `${data.location}`; 
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  // se corrige el nombre de la variable n a $n
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski').catch(handleError); 
